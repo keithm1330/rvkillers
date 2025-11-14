27813aeb55e7dd67d05444b6ced79b30973fe389
keithm1330 <keith13@live.ie>
2025-11-14 14:15:20 +0000

    first commit

---
+++ killerapp/src/main/java/rv/killers/killerapp/KillerappApplication.java
@@ -0,0 +1,13 @@
+package rv.killers.killerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KillerappApplication.class, args);
+	}
+
+}
---
+++ killerapp/src/test/java/rv/killers/killerapp/KillerappApplicationTests.java
@@ -0,0 +1,13 @@
+package rv.killers.killerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
